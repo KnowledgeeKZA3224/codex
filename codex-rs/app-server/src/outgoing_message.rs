877f539e6196737fdfdd2c8cab8a5cec652910fb
Knowledgee KZA (Architect) <erobles3224@gmail.com>
2026-09-18 08:01:34 -0700

    Import warning notification in barrier regression test

--- codex-rs/app-server/src/outgoing_message.rs
+++ codex-rs/app-server/src/outgoing_message.rs
@@ -1066,6 +1066,7 @@ mod tests {
     use codex_app_server_protocol::ServerResponse;
     use codex_app_server_protocol::ToolRequestUserInputParams;
     use codex_app_server_protocol::TurnModerationMetadataNotification;
+    use codex_app_server_protocol::WarningNotification;
     use codex_protocol::ThreadId;
     use pretty_assertions::assert_eq;
     use serde_json::json;
